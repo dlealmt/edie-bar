aa9decab6a3926526cdb8c875e69b6109e07c647
David Leal <david@davidleal.com>
2023-01-10 19:37:21 +0000

    [ml] Create svg node with dom-node

--- edie-ml.el
+++ edie-ml.el
@@ -46,13 +46,14 @@
   (pcase-let* ((edie-ml-unit-x (or edie-ml-unit-x (frame-char-width)))
                (edie-ml-unit-y (or edie-ml-unit-y (frame-char-height)))
                ((map height width) attributes))
-    `(svg
-      ((width . ,(or (and width (* width edie-ml-unit-x)) (frame-pixel-width)))
+    (dom-node
+     'svg
+     `((width . ,(or (and width (* width edie-ml-unit-x)) (frame-pixel-width)))
        (height . ,(or (and height (* height edie-ml-unit-y)) (frame-pixel-height)))
        (version . "1.1")
        (xmlns . "http://www.w3.org/2000/svg")
        (xmlns:xlink . "http://www.w3.org/1999/xlink"))
-      ,(edie-ml-parse spec))))
+     (edie-ml-parse spec))))
 
 (defun edie-ml-render (attributes spec)
   (let ((svg (edie-ml attributes spec)))
